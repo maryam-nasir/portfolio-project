30b8cb3966bf45c436c23038ca0f9c9922441034
Maryam Nasir <maryamnasir555@yahoo.com>
2024-12-18 15:46:47 +0500

    Added skill hover colors

---
+++ tailwind/colors.ts
@@ -0,0 +1,19 @@
+const COLORS = {
+  javascript: "#E8D91A",
+  typescript: "#3278C6",
+  react: "#62DAFB",
+  next: "#000000",
+  html5: "#F16528",
+  css3: "#1675BB",
+  "tailwind-css": "#38B7F0",
+  redux: "#764BBC",
+  "react-query": "#F84053",
+  "framer-motion": "#E700B6",
+  "react-native": "#62DAFB",
+  python: "#F1C941",
+  sql: "#339ADC",
+  git: "#F05033",
+  docker: "#2591E7",
+};
+
+export default COLORS;
